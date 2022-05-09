d487e6653bb0cf2a87ad8555a5568f49e1a7f3c8
Erison Felipe <erison274@gmail.com>
2022-05-09 19:28:50 -0300

    landpage/criacao-card

--- landpage/src/Components/Footer.js
+++ landpage/src/Components/Footer.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 import './Footer.css';
 import Logo from './image/logo.png';
 
@@ -7,7 +8,21 @@ export default class Footer extends Component {
         return (
             <div className="footer">
                  <footer class="rodape">
-                    <img class="rodape-img" src={Logo} alt="Logo" height="100px"/>
+                     <div class="img-logo">
+                        <img class="rodape-img" src={Logo} alt="Logo" height="100px"/>
+                     </div>
+                     <div class="icon">
+                        <div class="insta">
+                            <p><FaInstagram/> Siga</p>
+                        </div>
+                        <div class="whats">
+                            <p><FaWhatsapp/> Acompanhe</p>
+                        </div>                        
+                    </div>
+                    <div class="created">
+                        <p>Creado por: <a href="">Erison Felipe</a></p>
+                        
+                    </div>                    
                  </footer>
             </div>
         )
--- landpage/src/Components/Main.js
+++ landpage/src/Components/Main.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import './Main.css';
 import Logo from './image/logo.png';
 import Modal from './Modal';
+import Card from './Card';
 export default class Main extends Component {
     render(){
         return(
@@ -16,19 +17,8 @@ export default class Main extends Component {
             <img class="conteudo-principal-img" src={Logo} height="250px" alt="Imagem do Logo"/>
         </section>
         <section class="conteudo-secundario">
-            <h3 class="conteudo-secundario-titulo">Como posso te ajudar?</h3>
-            <p class="conteudo-secundario-escrito">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-                quam voluptatem ex fugiat. Culpa
-                corrupti voluptate quos voluptates harum, molestiae quisquam deleniti sequi nostrum nam. Magnam velit
-                unde esse eum!</p>
-            <p class="conteudo-secundario-escrito">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, eum!
-                Architecto exercitationem porro fuga
-                esse dolore officia, vel sunt placeat totam temporibus culpa corporis! Doloremque molestiae modi sit a
-                id!</p>
-            <p class="conteudo-secundario-escrito">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
-                quas non cum repudiandae minus
-                labore. Ratione porro accusantium numquam saepe sapiente dicta adipisci voluptatem asperiores incidunt
-                maxime, magnam labore aut!</p>
+            <h3 class="conteudo-secundario-titulo">Depoimentos</h3>
+            <Card></Card>
         </section>
     </main> 
             </div>
